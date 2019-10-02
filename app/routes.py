aa90cd5dd8ccf2d1bbc38f83d5e5efa47761251b
yonghyuc <yonghyuc@usc.edu>
2019-10-01 23:25:21 -0700

    init commit

---
+++ app/routes.py
@@ -0,0 +1,51 @@
+from app import app, api
+from flask import render_template
+from flask_restful import Resource
+from flask import request
+
+import base64
+from collections import defaultdict
+import os
+
+
+load_id_dict = defaultdict(lambda: -1)
+save_id_dict = defaultdict(lambda: -1)
+
+
+@app.route("/")
+@app.route("/index")
+def index():
+    return "HELLO WORLD!"
+
+
+@app.route("/player/<int:id>")
+def player_1(id):
+    return render_template('player.html', id=id)
+
+
+class Image(Resource):
+    def get(self, id):
+        return f'this is the GET {id}'
+
+    def post(self, id):
+        image_data = request.data
+
+        if not os.path.exists(os.path.dirname(f"./app/resources/{id}/")):
+            try:
+                os.makedirs(os.path.dirname(f"./app/resources/{id}/"))
+            except OSError as exc: # Guard against race condition
+                return 'Fail to make a directory'
+
+        trimmed_img_base64 = image_data.decode("utf-8").replace('data:image/png;base64,', '')
+        binary_img = base64.b64decode(trimmed_img_base64)
+
+        save_id_dict[id] += 1
+        with open(f"./app/resources/{id}/{save_id_dict[id]}.png", "wb+") as fh:
+            fh.write(binary_img)
+
+        return f'this is the POST {id}'
+
+    def delete(self, id):
+        pass
+
+api.add_resource(Image, '/player/image/<int:id>')
